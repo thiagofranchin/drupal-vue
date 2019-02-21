862200b6e9b018a29bd1dbea9fe88f780eb0eee3
Thiago Chaves de Camargo Franchin <tfranchin@lnxcit020031.cit>
2019-02-21 14:58:39 -0300

    test

--- precache-manifest.893815a88dd4fa51381ce39f175916d4.js
+++ precache-manifest.893815a88dd4fa51381ce39f175916d4.js
@@ -1,30 +1,30 @@
 self.__precacheManifest = [
   {
     "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
-    "url": "/robots.txt"
+    "url": "robots.txt"
   },
   {
     "revision": "eba30ba7d72231b5b976",
-    "url": "/js/chunk-vendors.beca3200.js"
+    "url": "js/chunk-vendors.beca3200.js"
   },
   {
     "revision": "785e1b9b7ffda83ad84e",
-    "url": "/js/app.7bce5b2a.js"
+    "url": "js/app.7bce5b2a.js"
   },
   {
     "revision": "96e439750ba63147854d98f9fb94fad6",
-    "url": "/index.html"
+    "url": "index.html"
   },
   {
     "revision": "404c221ccb8eb4f8710a6fb160e971d4",
-    "url": "/img/cit.404c221c.png"
+    "url": "img/cit.404c221c.png"
   },
   {
     "revision": "eba30ba7d72231b5b976",
-    "url": "/css/chunk-vendors.1b8a695f.css"
+    "url": "css/chunk-vendors.1b8a695f.css"
   },
   {
     "revision": "785e1b9b7ffda83ad84e",
-    "url": "/css/app.e9d07031.css"
+    "url": "css/app.e9d07031.css"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.893815a88dd4fa51381ce39f175916d4.js"
+  "precache-manifest.893815a88dd4fa51381ce39f175916d4.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "frontend2"});
